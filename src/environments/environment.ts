656efcc3e6b2421e9595c09c54e8a329e9af2901
Everton <evertonbueno2014@gmail.com>
2024-11-29 00:39:37 -0300

    asdadas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl:'ec2-3-139-87-142.us-east-2.compute.amazonaws.com:8080/api'
+  baseUrl:'http://ec2-3-139-87-142.us-east-2.compute.amazonaws.com:8080/api'
 }
 
 
